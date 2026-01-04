fda43303e7de187040d9076cdd05d70958c2c84c
Tolga Erdogmus <tolgaerdogmus@gmail.com>
2026-01-04 15:08:53 +0300

    first movings

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
+def move_forwards():
+    tim.forward(10)
+
+
 screen.listen()
-# bzztt
+screen.onkey(key="space", fun=move_forwards)
+screen.exitonclick()
