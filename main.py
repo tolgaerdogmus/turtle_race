91e3a824922d23f7b6a1dacd17ceabc0bb8170a3
Tolga Erdogmus <tolgaerdogmus@gmail.com>
2026-01-04 15:32:24 +0300

    reset function

--- main.py
+++ main.py
@@ -17,9 +17,16 @@ def turn_right():
     new_heading = tim.heading() - 10 # tim.right(10)
     tim.setheading(new_heading)
 
+def reset():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
 screen.listen()
 screen.onkey(move_forwards, "g")
 screen.onkey(move_backwards, "i")
 screen.onkey(turn_left, "u")
 screen.onkey(turn_right, "e")
+screen.onkey(reset, "c")
 screen.exitonclick()
