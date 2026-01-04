ee85123db1ba192d92bbd215e84f691d95cacae5
Tolga Erdogmus <tolgaerdogmus@gmail.com>
2026-01-04 15:28:20 +0300

    move and draw func

--- main.py
+++ main.py
@@ -6,7 +6,20 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10 # tim.left(10)
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10 # tim.right(10)
+    tim.setheading(new_heading)
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "g")
+screen.onkey(move_backwards, "i")
+screen.onkey(turn_left, "u")
+screen.onkey(turn_right, "e")
 screen.exitonclick()
